a20da0ec8b24e395c4ee079d58ae167696701212
iparth0611 <prbshah49@gmail.com>
2022-01-16 20:55:20 -0500

    Adding files

---
+++ Parth_Shah_module1.R
@@ -0,0 +1,60 @@
+#Name
+print("Parth Shah")
+
+#Install vcd package
+r=getOption("repos")
+r["CRAN"]="https://cran.r-project.org/"
+options(repos=r)
+install.packages("vcd")
+
+#Import vcd library
+library(vcd)
+
+#Sales data
+Sales <- c(7, 11, 15, 20, 19, 11, 18, 10, 6, 22)
+
+#Temperature data 
+Temperature <- c(69, 81, 77, 84, 80, 97, 87, 70, 65, 90)
+
+#Plot sales ~ Temperature
+plot(Sales ~  Temperature)
+
+
+#Mean
+mean(Temperature)
+
+#Remove element
+Sales <- Sales[-3]
+Sales
+
+#Insert element 
+Sales <- c(Sales[1:2], 16, Sales[3:9])
+Sales
+
+#Create name in  vector
+name <- c("Tom", "Dick", "Harry")
+name
+
+#Creating matrix 
+p <- matrix(1:10, nrow =5, ncol =2)
+View(p)
+
+#Dataframes
+icSales <- data.frame(Sales, Temperature)
+icSales
+
+#Dataframe structure 
+structure(icSales)
+
+#Dataframe summary
+summary(icSales)
+
+#Import data
+library(readr)
+Student <- read_csv("R/ALY6000/Module1/Student.csv")
+View(Student)
+#~/R/ALY6000/Module1/Student.csv
+
+#display names of students
+ls(Student)
+
